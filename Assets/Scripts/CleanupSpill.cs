d3de631dd9d15d6560638f0863153f67ed68f793
Carlos <123418797+Cegr7150@users.noreply.github.com>
2025-09-28 13:27:48 -0600

    Added Cleanup Spill
    added cleanup spill script

---
+++ Assets/Scripts/CleanupSpill.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private bool playerInside = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInside && Input.GetKeyDown(KeyCode.F))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
