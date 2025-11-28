244a15808f8f9128c4f5055840dbd9aee64325c8
Connor Walton <46007308+Ilovetoocode@users.noreply.github.com>
2025-11-27 19:10:11 -0700

    Random announcement system!

--- Assets/Scripts/PA_Script.cs
+++ Assets/Scripts/PA_Script.cs
@@ -1,20 +1,31 @@
 using UnityEngine;
-
+using System.Collections;
 public class PA_Script : MonoBehaviour
 {
 AudioSource Muzak;
 AudioSource Announcements;
+public float AudioTime = 0.00f;
+public float MinAudioTime  = 60.00f;
+public float MaxAudioTime  = 180.00f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+void SetTimer(){
+AudioTime=Random.Range(MinAudioTime,MaxAudioTime);
+}
     void Start()
     {
           AudioSource[] audios = GetComponents<AudioSource>();
     Muzak = audios[0];
     Announcements = audios[1];
+    Muzak.Play();
+    SetTimer();
     }
-
     // Update is called once per frame
     void Update()
     {
-    
+    AudioTime -= Time.deltaTime;
+    if(AudioTime <= 0){
+        Announcements.Play();
+        SetTimer();
+    }
     }
 }
